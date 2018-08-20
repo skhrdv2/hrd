bdefa4317f1353778edae52537e91cca183dfa70
diedmanz <diedmanz@gmail.com>
2018-08-20 11:03:34 +0700

    แก้ภาษาให้คนไทย

--- login.php
+++ login.php
@@ -118,13 +118,13 @@ $Db = new MySqlConn;
                                             <div>
                                                 <a href="#" data-target="#forgot-box" class="forgot-password-link">
                                                     <i class="ace-icon fa fa-arrow-left"></i>
-                                                    I forgot my password
+                                                    ลืมรหัสผ่าน
                                                 </a>
                                             </div>
 
                                             <div>
                                                 <a href="#" data-target="#signup-box" class="user-signup-link">
-                                                    I want to register
+                                                    สมัครสมาชิก
                                                     <i class="ace-icon fa fa-arrow-right"></i>
                                                 </a>
                                             </div>
