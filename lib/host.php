501c1862005300694647392aef872a3d82b9ec0c
skhrdv2 <42473818+skhrdv2@users.noreply.github.com>
2018-09-01 08:17:36 +0700

    dd

--- lib/host.php
+++ lib/host.php
@@ -3,6 +3,8 @@ date_default_timezone_set("Asia/Bangkok");
 define("host", "localhost");
 define("username", "root");
 define("password", "12345678");
+define("username", "sa");
+
 define("db", "hrd");
 
 ?>
