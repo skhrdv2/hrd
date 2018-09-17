7eadd4ea8a724ee51deb53031ae123595689990f
DESKTOP-HAQSS2E\yeawnote <yeawyow@gmail.com>
2018-09-17 13:38:10 +0700

    แก้ host ok

--- lib/host.php
+++ lib/host.php
@@ -1,15 +1,8 @@
 <?php
 date_default_timezone_set("Asia/Bangkok"); 
-define("host", "localhost");
-<<<<<<< HEAD
-define("username", "sa");
-define("password", "sa");
-=======
-define("username", "root");
+define("host", "202.80.228.36");
+define("username", "skhrd");
 define("password", "12345678");
-define("username", "sa");
-
->>>>>>> 35a805fe85a01547ad8929061c8639128f86f037
 define("db", "hrd");
 
 ?>
